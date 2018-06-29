cb25cd2ae86f7a4d3b8cebabf1229f756736c521
ChenxiYuan <yuanchenxi95@gmail.com>
2018-06-29 18:26:38 +0800

    add env variable

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -4,6 +4,8 @@ const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 const merge = require('webpack-merge')
 
 let api_end_point = process.env['API_END_POINT']
+let app_key = process.env['APP_KEY']
+let app_secret = process.env['APP_SECRET']
 
 module.exports = merge(common, {
   mode: 'production',
@@ -14,6 +16,8 @@ module.exports = merge(common, {
     new webpack.DefinePlugin({
       'process.env': {
         'API_END_POINT': JSON.stringify(api_end_point),
+        'APP_KEY': JSON.stringify(app_key),
+        'APP_SECRET': JSON.stringify(app_secret),
       },
     }),
   ],
